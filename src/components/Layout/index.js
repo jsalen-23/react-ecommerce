c7e2499477603a5def3395d2683bf322c5fe05a3
Joseph Salen <jsalen.91@gmail.com>
2021-09-20 18:46:04 -0400

    create layout style

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,12 +1,14 @@
 import { Fragment } from 'react';
 import { Helmet } from 'react-helmet';
 
+import { Wrapper } from './styles';
+
 const Layout = ({ children, title }) => {
   return (
-    <Fragment>
+    <Wrapper>
       <Helmet>{title && <title>{title} | Salen-Store</title>}</Helmet>
       {children}
-    </Fragment>
+    </Wrapper>
   );
 };
 
---
+++ src/components/Layout/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  height: 100%;
+  margin: 16px 16px 76px;
+`;
