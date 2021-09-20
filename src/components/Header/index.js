641685f9b40fdb0cf731c8d54bbc93099abd8826
Joseph Salen <jsalen.91@gmail.com>
2021-09-20 12:49:27 -0400

    create Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import { Wrapper, Link, Title } from './styles';
+
+const PROFILE_IMG =
+  'https://pbs.twimg.com/profile_images/1339420099913134081/aKyJWnDK_400x400.jpg';
+
+const Header = () => {
+  return (
+    <Wrapper>
+      <Link to='/user'>
+        <img src={PROFILE_IMG} alt='Profile Picture' />
+      </Link>
+      <Title>Inicio</Title>
+    </Wrapper>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/styles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+import { Link as LinkRouter } from 'react-router-dom';
+
+export const Wrapper = styled.header`
+  width: 100%;
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 18px;
+  border-bottom: 1px solid #2c3841;
+`;
+
+export const Link = styled(LinkRouter)`
+  width: 35px;
+  height: 35px;
+  overflow: hidden;
+  border-radius: 50%;
+  & img {
+    width: 35px;
+    height: 35px;
+    object-fit: cover;
+  }
+`;
+
+export const Title = styled.h1`
+  font-size: 19px;
+  font-weight: 700;
+  line-height: 22px;
+`;
