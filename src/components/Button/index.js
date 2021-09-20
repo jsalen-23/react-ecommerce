5d7ddb6e55d3b145c0f1b3c6b15d11f607678554
Joseph Salen <jsalen.91@gmail.com>
2021-09-20 16:53:33 -0400

    create Button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,7 @@
+import { Btn } from './styles';
+
+const Button = ({ children }) => {
+  return <Btn>{children}</Btn>;
+};
+
+export default Button;
---
+++ src/components/Button/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Btn = styled.button`
+  width: 100%;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #1d9bf0;
+  border-radius: 4px;
+  box-shadow: 0 3px 6px 0 rgb(0 0 0 / 16%);
+  color: #fff;
+  font-size: 14px;
+  font-weight: 700;
+  text-transform: uppercase;
+
+  & svg {
+    margin-left: 4px;
+  }
+`;
