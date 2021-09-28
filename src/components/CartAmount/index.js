c9672f3a8340f33d4220e7e5bedbdf4ba678a521
Joseph Salen <jsalen.91@gmail.com>
2021-09-28 19:19:38 -0400

    create Cart Amount component

---
+++ src/components/CartAmount/index.js
@@ -0,0 +1,12 @@
+import { Wrapper, Title, Price } from './styles';
+
+const CartAmount = () => {
+  return (
+    <Wrapper>
+      <Title>Cart Total:</Title>
+      <Price>$ 10.00</Price>
+    </Wrapper>
+  );
+};
+
+export default CartAmount;
---
+++ src/components/CartAmount/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.article`
+  width: 100%;
+  height: auto;
+  display: flex;
+  justify-content: flex-end;
+  align-items: baseline;
+  flex-wrap: wrap;
+  font-size: 16px;
+  margin: 0 8px;
+`;
+
+export const Title = styled.p`
+  margin-right: 12px;
+  font-weight: 500;
+`;
+
+export const Price = styled.p`
+  font-size: 22px;
+  font-weight: 700;
+  color: #1d9bf0;
+`;
