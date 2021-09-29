a2eb95accc0977a2b58461b99aca917a3ce7ace7
Joseph Salen <jsalen.91@gmail.com>
2021-09-29 16:06:35 -0400

    create styles to page Cart

---
+++ src/pages/Cart/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Footer = styled.footer`
+  height: 112px;
+  position: fixed;
+  right: 8px;
+  bottom: 58px;
+  left: 8px;
+  z-index: 3;
+  background-color: #15202b;
+`;
+
+export const BtnWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const DetailWrapper = styled.div`
+  padding-bottom: 100px;
+`;
