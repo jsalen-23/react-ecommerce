ffd5f28c1667ff19d966daa36359f45609b6ff80
Joseph Salen <jsalen.91@gmail.com>
2021-09-29 16:01:26 -0400

    get total amount from context app

--- src/components/CartAmount/index.js
+++ src/components/CartAmount/index.js
@@ -1,10 +1,18 @@
+import { useContext } from 'react';
+import { AppContext } from '../../context/AppContext';
 import { Wrapper, Title, Price } from './styles';
 
 const CartAmount = () => {
+  const {
+    state: {
+      cart: { total },
+    },
+  } = useContext(AppContext);
+
   return (
     <Wrapper>
       <Title>Cart Total:</Title>
-      <Price>$ 10.00</Price>
+      <Price>$ {total.toFixed(2)}</Price>
     </Wrapper>
   );
 };
