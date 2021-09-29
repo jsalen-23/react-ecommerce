c69c167104f8b72e87e0a7a7783ff6822dc7b8c7
Joseph Salen <jsalen.91@gmail.com>
2021-09-29 16:05:41 -0400

    implement Cart page logic

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,9 +1,52 @@
+import { useContext } from 'react';
+import { AppContext } from '../../context/AppContext';
 import Layout from '../../components/Layout';
+import CartAmount from '../../components/CartAmount';
+import CartEmpty from '../../components/CartEmpty';
+import DetailCard from '../../components/DetailCard';
+import Button from '../../components/Button';
+
+import { MdArrowForward } from 'react-icons/md';
+import { Footer, BtnWrapper, DetailWrapper } from './styles';
 
 const Cart = () => {
+  const {
+    state: {
+      cart: { products },
+    },
+    removeFromCart,
+  } = useContext(AppContext);
+
   return (
     <Layout title='Cart'>
-      <p>Search</p>
+      {products.length === 0 ? (
+        <CartEmpty />
+      ) : (
+        <>
+          <DetailWrapper>
+            {products.map((product) => (
+              <div key={product.id}>
+                <DetailCard {...product} />
+                <BtnWrapper>
+                  <Button
+                    size='sm'
+                    variant='warning'
+                    onClick={() => removeFromCart(product)}
+                  >
+                    Delete
+                  </Button>
+                </BtnWrapper>
+              </div>
+            ))}
+          </DetailWrapper>
+          <Footer>
+            <CartAmount />
+            <Button size='lg'>
+              Checkout <MdArrowForward size='18' />
+            </Button>
+          </Footer>
+        </>
+      )}
     </Layout>
   );
 };
