d02e626758f5293f7cfc4127b79d812c1fb43ed9
Joseph Salen <jsalen.91@gmail.com>
2021-10-05 21:09:12 -0400

    add action to add and remove from favorites

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -3,15 +3,34 @@ import { AppContext } from '../../context/AppContext';
 import Button from '../Button';
 
 import { Article, CardTitle, CardPrice, ImageWrapper, Footer } from './styles';
-import { MdArrowForward, MdFavoriteBorder } from 'react-icons/md';
+import { MdArrowForward, MdFavoriteBorder, MdFavorite } from 'react-icons/md';
 
 const ProductCard = ({ product }) => {
-  const { addToCart, addToFavorites } = useContext(AppContext);
+  const {
+    state: { favorites },
+    addToCart,
+    addToFavorites,
+    removeFromFavorites,
+  } = useContext(AppContext);
+
+  const checkFavorites = (product) => {
+    const isFavorite = favorites.includes(product);
+
+    if (isFavorite) {
+      return (
+        <MdFavorite size='26' onClick={() => removeFromFavorites(product)} />
+      );
+    } else {
+      return (
+        <MdFavoriteBorder size='26' onClick={() => addToFavorites(product)} />
+      );
+    }
+  };
 
   return (
     <Article>
       <ImageWrapper>
-        <MdFavoriteBorder size='26' onClick={() => addToFavorites(product)} />
+        {checkFavorites(product)}
         <img src={product.image} alt={product.title} />
       </ImageWrapper>
       <Footer>
