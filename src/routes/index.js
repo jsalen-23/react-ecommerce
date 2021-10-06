28736c9b0f2a86b8751c0724bbc954364c0f1c49
Joseph Salen <jsalen.91@gmail.com>
2021-10-06 19:37:42 -0400

    add Lazy Load

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,32 +1,41 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import Loading from '../components/Loading';
 import Header from '../components/Header';
-import Navigation from '../components/Navigation';
-import Cart from '../pages/Cart';
-import Checkout from '../pages/Checkout';
-import Favorites from '../pages/Favorites';
 import Home from '../pages/Home';
-import Payment from '../pages/Payment';
-import PaymentResult from '../pages/PaymentResult';
+import Navigation from '../components/Navigation';
+
+const Cart = lazy(() => import('../pages/Cart'));
+const Checkout = lazy(() => import('../pages/Checkout'));
+const Favorites = lazy(() => import('../pages/Favorites'));
+const Payment = lazy(() => import('../pages/Payment'));
+const PaymentResult = lazy(() => import('../pages/PaymentResult'));
 
 const Routes = () => {
   return (
     <Router>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/cart' component={Cart} />
-        <Route exact path='/favorites' component={Favorites} />
-        <Route exact path='/checkout' component={Checkout} />
-        <Route exact path='/checkout/payment' component={Payment} />
-        <Route
-          exact
-          path='/checkout/payment/success/:orderID'
-          component={PaymentResult}
-        />
-        <Route exact path='/checkout/payment/fail' component={PaymentResult} />
-      </Switch>
-      <Navigation />
+      <Suspense fallback={<Loading />}>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/cart' component={Cart} />
+          <Route exact path='/favorites' component={Favorites} />
+          <Route exact path='/checkout' component={Checkout} />
+          <Route exact path='/checkout/payment' component={Payment} />
+          <Route
+            exact
+            path='/checkout/payment/success/:orderID'
+            component={PaymentResult}
+          />
+          <Route
+            exact
+            path='/checkout/payment/fail'
+            component={PaymentResult}
+          />
+        </Switch>
+        <Navigation />
+      </Suspense>
     </Router>
   );
 };
