c7806226a4ac5ab733332e65b2de5243cfd14587
Joseph Salen <jsalen.91@gmail.com>
2021-10-06 22:38:32 -0400

    fix grid to max width 390px

--- src/components/ListOfProducts/styles.js
+++ src/components/ListOfProducts/styles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.section`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(320px, 390px));
+  justify-content: center;
   gap: 20px;
 `;
