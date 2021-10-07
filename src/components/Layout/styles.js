284da1d9dfce6adee5ce8f44ee61d5d0426c9dd2
Joseph Salen <jsalen.91@gmail.com>
2021-10-06 21:48:41 -0400

    add styling for laptop

--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -1,6 +1,12 @@
 import styled from 'styled-components';
+import { device } from '../../mediaQuery';
 
 export const Wrapper = styled.div`
   height: 100%;
   margin: 16px 8px 76px;
+
+  @media ${device.laptop} {
+    margin-left: 88px;
+    padding-left: 20px;
+  }
 `;
