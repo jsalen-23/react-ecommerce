baa57cd1be682a412bb1f31f0762b9211ef27645
Joseph Salen <jsalen.91@gmail.com>
2021-10-06 21:38:03 -0400

    create mediaQueries

---
+++ src/mediaQuery/index.js
@@ -0,0 +1,13 @@
+const size = {
+  tablet: '768px',
+  laptop: '1024px',
+  laptopL: '1440px',
+  desktop: '2560px',
+};
+
+export const device = {
+  tablet: `(min-width: ${size.tablet})`,
+  laptop: `(min-width: ${size.laptop})`,
+  laptopL: `(min-width: ${size.laptopL})`,
+  desktop: `(min-width: ${size.desktop})`,
+};
