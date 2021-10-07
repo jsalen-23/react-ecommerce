420d078d0553ede470d5c2cb8eca5697cb66de6e
Joseph Salen <jsalen.91@gmail.com>
2021-10-06 22:06:04 -0400

    refactor with new ListOfProducts component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,27 +1,12 @@
-import { useContext } from 'react';
 import Layout from '../../components/Layout';
-import ProductCard from '../../components/ProductCard';
 import FilterBar from '../../components/FilterBar';
-
-import { AppContext } from '../../context/AppContext';
-import Error from '../../components/Error';
-import Loading from '../../components/Loading';
+import ListOfProducts from '../../components/ListOfProducts';
 
 const Home = () => {
-  const { products, loading, error } = useContext(AppContext);
-
-  if (error) return <Error />;
-
   return (
     <Layout title='Home'>
       <FilterBar />
-      {loading ? (
-        <Loading />
-      ) : (
-        products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))
-      )}
+      <ListOfProducts />
     </Layout>
   );
 };
