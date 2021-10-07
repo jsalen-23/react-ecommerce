45a917763510d289710e783cd66a8ce11b3df6f1
Joseph Salen <jsalen.91@gmail.com>
2021-10-06 22:10:36 -0400

    create grid

--- src/components/ListOfProducts/styles.js
+++ src/components/ListOfProducts/styles.js
@@ -1,3 +1,6 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.section``;
+export const Wrapper = styled.section`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+`;
