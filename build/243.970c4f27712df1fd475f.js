36bef04635321d2c6498fac5dde47b7ad6a312ba
Joseph Salen <jsalen.91@gmail.com>
2021-10-08 20:11:01 -0400

    build to vercel 0.1

---
+++ build/243.970c4f27712df1fd475f.js
@@ -0,0 +1,35 @@
+"use strict";(self.webpackChunksalen_store=self.webpackChunksalen_store||[]).push([[243],{2243:(t,e,a)=>{a.r(e),a.d(e,{default:()=>x});var i=a(7294),n=a(9816),p=a(9163),s=a(1249);const o=p.ZP.article`
+  width: 100%;
+  height: auto;
+  display: flex;
+  justify-content: flex-end;
+  align-items: baseline;
+  flex-wrap: wrap;
+  margin: 12px 0;
+
+  @media ${s.U.laptop} {
+    padding-right: 74px;
+    padding-bottom: 36px;
+  }
+
+  @media ${s.U.laptopL} {
+    padding-right: 221px;
+  }
+`,l=p.ZP.p`
+  margin-right: 12px;
+  font-size: 17px;
+  font-weight: 500;
+
+  @media ${s.U.laptop} {
+    font-size: 24px;
+  }
+`,r=p.ZP.p`
+  font-size: 22px;
+  font-weight: 700;
+  color: #1d9bf0;
+
+  @media ${s.U.laptop} {
+    font-size: 24px;
+  }
+`;var d=a(5893);const x=()=>{const{state:{cart:{total:t}}}=(0,i.useContext)(n.I);return(0,d.jsxs)(o,{children:[(0,d.jsx)(l,{children:"Cart Total:"}),(0,d.jsxs)(r,{children:["$ ",t.toFixed(2)]})]})}}}]);
+//# sourceMappingURL=243.970c4f27712df1fd475f.js.map
